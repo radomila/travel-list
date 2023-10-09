5caa9b4e303fe30739c866ab611607afe3abcb34
Radomila Hejhalová <hejhalova.radomila@gmail.com>
2023-10-09 10:43:54 +0200

    feat: add layout and rendering static list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './index.css';
 
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +30,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>X</button>
+    </li>
+  );
 }
 
 function Stats() {
